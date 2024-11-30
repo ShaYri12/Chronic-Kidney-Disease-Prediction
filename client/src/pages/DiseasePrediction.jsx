11642d15666839a42d1a9f23c63ee53a016a9579
ShaYri12 <sharjeelb60@gmail.com>
2024-11-30 05:15:54 +0500

    deploy fix

--- client/src/pages/DiseasePrediction.jsx
+++ client/src/pages/DiseasePrediction.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./disease-prediction.css";
 import { Link, useNavigate } from "react-router-dom";
 import LoginImg from "../../public/Images/newsletter.gif";
