dee3f80fba7b3f52d548851e59959169a7eeacd6
ShaYri12 <sharjeelb60@gmail.com>
2024-11-30 06:03:25 +0500

    cors issue fixing

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+
 const api = axios.create({
   baseURL: "https://chronic-kidney-disease-prediction-backend.vercel.app/api",
+  withCredentials: true, // Set credentials to true globally for all requests
 });
 
 export default api;
