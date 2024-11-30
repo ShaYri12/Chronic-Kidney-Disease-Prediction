b0c22d78fc68ed4d5864a0a1a472cde85a81745b
ShaYri12 <sharjeelb60@gmail.com>
2024-12-01 03:05:51 +0500

    cors

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "chronic-kidney-disease-p-git-21d56e-sharjeels-projects-c1e8a39b.vercel.app/api",
+    "https://chronic-kidney-disease-p-git-21d56e-sharjeels-projects-c1e8a39b.vercel.app/api",
   withCredentials: true, // Set credentials to true globally for all requests
 });
 
